d21a591839428c9d0bf206bf373ae3f84d0803fa
Philip Kin <philip@thekinfamily.com>
2016-11-23 10:51:41 -0600

    Cleaning up code and moveing parsing logic. Slightly more functional code style.

---
+++ blotterPageParser.js
@@ -0,0 +1,112 @@
+module.exports = exports = {};
+
+function cloneContext(parserContext) {
+  return JSON.parse(JSON.stringify(parserContext));
+}
+
+function completeCaseInfo(parserContext) {
+  const context = cloneContext(parserContext);
+
+  if(context.caseInfo) {
+    if(context.caseInfo.offenseType == "") context.caseInfo.offenseType = null;
+    if(context.caseInfo.offenseDescription == "") context.caseInfo.offenseDescription = null;
+    if(context.caseInfo.caseNumber == "") context.caseInfo.caseNumber = null;
+    if(context.caseInfo.dispositionDateTime == "") context.caseInfo.dispositionDateTime = null;
+    if(context.caseInfo.disposition == "") context.caseInfo.disposition = null;
+    context.docket.cases.push(context.caseInfo);
+  }
+
+  context.caseInfo = {
+    offenseType: sortedText[i].text,
+    offenseDescription: "",
+    caseNumber: "",
+    dispositionDateTime: "",
+    disposition: ""
+  }
+
+  return context;
+}
+
+function completeDocket(parserContext) {
+  var context = cloneContext(parserContext);
+  if(context.docket) {
+    context = completeCaseInfo(context);
+    context.dockets.push(context.docket);
+    context.docket = {
+      cases: []
+    };
+    context.caseInfo = null;
+    context.caseInfoSection = false;
+  }
+
+  return context;
+}
+
+module.exports.parsePage = function(page, parserContext) {
+  var context = Object.assign({
+    docket: null,
+    caseInfo: null,
+    dockets: [],
+    caseInfoSection: false,
+
+    caseTable: {
+      offenseTypeX: 0,
+      offenseDescriptionX: 0,
+      caseNumberX: 0,
+      dispositionDateTimeX: 0,
+      dispositionX: 0
+    }
+  }, cloneContext(parserContext));
+
+  var sortedText = page.Texts.sort((a,b) => a.y != b.y ? a.y - b.y : a.x - b.x).map(t => ({ text: urldecode(t.R[0].T), x: t.x, y: t.y }));
+  for(var i=0;i<sortedText.length;i++){
+    if(sortedText[i].text == "Docket:") {
+      context = completeDocket(context);
+    }
+
+    if(sortedText[i].text.endsWith(":") && sortedText.length > i + 1 && !context.caseInfoSection) {
+      context.docket[sortedText[i].text.replace(":", "")] = sortedText[i + 1].text.endsWith(":") ? null : sortedText[i + 1].text;
+    }
+
+    if(sortedText[i].text == "Offense Type"){
+      context.caseInfoSection = true;
+      context.caseTable.offenseTypeX = sortedText[i].x;
+    }
+    else if(sortedText[i].text == "Offense Description"){
+      context.caseTable.offenseDescriptionX = sortedText[i].x;
+    }
+    else if(sortedText[i].text == "Case Number"){
+      context.caseTable.caseNumberX = sortedText[i].x;
+    }
+    else if(sortedText[i].text == "Disposition Date/Time"){
+      context.caseTable.dispositionDateTimeX = sortedText[i].x;
+    }
+    else if(sortedText[i].text == "Disposition") {
+      context.caseTable.dispositionX = sortedText[i].x;
+    }
+    else if(context.caseInfoSection && sortedText[i].x == context.caseTable.offenseTypeX) {
+      if(sortedText[i].text == "Totals By Booking Type") {
+        context.caseInfoSection = false;
+      }
+      context = completeCaseInfo(context);
+    }
+    else if(context.caseInfoSection && sortedText[i].x == context.caseTable.offenseDescriptionX) {
+      context.caseInfo.offenseDescription += sortedText[i].text;
+    }
+    else if(context.caseInfoSection && sortedText[i].x == context.caseTable.caseNumberX) {
+      context.caseInfo.caseNumber += sortedText[i].text;
+    }
+    else if(context.caseInfoSection && sortedText[i].x == context.caseTable.dispositionDateTimeX) {
+      context.caseInfo.dispositionDateTime += sortedText[i].text;
+    }
+    else if(context.caseInfoSection && sortedText[i].x == context.caseTable.dispositionX) {
+      context.caseInfo.disposition += sortedText[i].text;
+    }
+  }
+
+  return context;
+}
+
+module.exports.completeParse = function(parserContext) {
+  return completeDocket(parserContext).dockets;
+}
--- blotterParser.js
+++ blotterParser.js
@@ -1,103 +1,23 @@
-let PDFParser = require("pdf2json");
-let urldecode = require("urldecode"); 
+const PDFParser = require("pdf2json");
+const urldecode = require("urldecode");
+const pageParser = require("./blotterPageParser");
+
 
 module.exports = function(data) {
   return new Promise(function(resolve, reject) {
-let pdfParser = new PDFParser();
+    const pdfParser = new PDFParser();
 
-pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-pdfParser.on("pdfParser_dataReady", pdfData => {
-  var dockets = [];
-  var docket = null;
-  var caseInfo = null;
-  var caseInfoSection = false;
-
-  var offenseTypeX = 0;
-  var offenseDescriptionX = 0;
-  var caseNumberX = 0;
-  var dispositionDateTimeX = 0;
-  var dispositionX = 0;
-
-  for(var page=0;page<pdfData.formImage.Pages.length;page++)
-  {
-    var sortedText = pdfData.formImage.Pages[page].Texts.sort((a,b) => a.y != b.y ? a.y - b.y : a.x - b.x).map(t => ({ text: urldecode(t.R[0].T), x: t.x, y: t.y }));
-
-    for(var i=0;i<sortedText.length;i++){
-      if(sortedText[i].text == "Docket:") {
-        if(docket != null) {
-          if(caseInfo != null) {
-            docket.cases.push(caseInfo);
-          }
-          dockets.push(docket);
-        }
-
-        docket = { cases: []};
-        caseInfoSection = false;
+    pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
+    pdfParser.on("pdfParser_dataReady", pdfData => {
+      var context = {};
+      for(var page=0;page<pdfData.formImage.Pages.length;page++)
+      {
+        context = pageParser.parsePage(pdfData.formImage.Pages[page], context);
       }
 
-      if(sortedText[i].text.endsWith(":") && sortedText.length > i + 1 && !caseInfoSection) {
-        docket[sortedText[i].text.replace(":", "")] = sortedText[i + 1].text.endsWith(":") ? null : sortedText[i + 1].text;
-      }
+      resolve(pageParser.completeParse(context));
+    });
 
-      if(sortedText[i].text == "Offense Type"){
-        caseInfoSection = true;
-        offenseTypeX = sortedText[i].x;
-      }
-      else if(sortedText[i].text == "Offense Description"){
-        offenseDescriptionX = sortedText[i].x;
-      }
-      else if(sortedText[i].text == "Case Number"){
-        caseNumberX = sortedText[i].x;
-      }
-      else if(sortedText[i].text == "Disposition Date/Time"){
-        dispositionDateTimeX = sortedText[i].x;
-      }
-      else if(sortedText[i].text == "Disposition") {
-        dispositionX = sortedText[i].x;
-      }
-      else if(caseInfoSection && sortedText[i].x == offenseTypeX) {
-        if(sortedText[i].text == "Totals By Booking Type") {
-          caseInfoSection = false;
-        }
-        else {
-          if(caseInfo != null) {
-            docket.cases.push(caseInfo);
-          }
-          caseInfo = {
-            offenseType: sortedText[i].text,
-            offenseDescription: "",
-            caseNumber: "",
-            dispositionDateTime: "",
-            disposition: ""
-          }
-        }
-        
-      }
-      else if(caseInfoSection && sortedText[i].x == offenseDescriptionX) {
-        caseInfo.offenseDescription += sortedText[i].text;
-      }
-      else if(caseInfoSection && sortedText[i].x == caseNumberX) {
-        caseInfo.caseNumber += sortedText[i].text;
-      }
-      else if(caseInfoSection && sortedText[i].x == dispositionDateTimeX) {
-        caseInfo.dispositionDateTime += sortedText[i].text;
-      }
-      else if(caseInfoSection && sortedText[i].x == dispositionX) {
-        caseInfo.disposition += sortedText[i].text;
-      }
-    }
-  }
-
-  if(docket != null){
-    if(caseInfo != null) {
-      docket.cases.push(caseInfo);
-    }
-    dockets.push(docket);
-  }
-
-  resolve(dockets);
-});
-
-pdfParser.parseBuffer(data);
+    pdfParser.parseBuffer(data);
   });
 }
