228fe826ec9e98c66117fdd961218dd5e4aae346
Philip Kin <philip@thekinfamily.com>
2016-12-02 21:47:34 -0600

    Cache requests for future use (for one hour)

--- controllers/DefaultService.js
+++ controllers/DefaultService.js
@@ -4,7 +4,9 @@ const loader = require("./reportLoader");
 const getRequestIP = require( 'get-request-ip' );
 const config = require('../config.js');
 const blotterPageParser = require("./blotterPageParser");
-const inmateBookingPageParser = require("./inmateBookingPageParser")
+const inmateBookingPageParser = require("./inmateBookingPageParser");
+const fs = require("fs");
+
 
 exports.deskBlotterGET = function(args, req, res, next) {
   res.setHeader('Content-Type', 'application/json');
@@ -12,8 +14,28 @@ exports.deskBlotterGET = function(args, req, res, next) {
         headers: []
     }).endsWith(x));
 
+
+  try{
+    var stat = fs.statSync("cache/cachedDeskBlotterResults" + scrub + ".json");
+    var ctime = new Date(stat.ctime);
+    var now = new Date();
+
+    if((now - ctime) > 3600000) {
+      console.log("old file.");
+      fs.unlinkSync("cache/cachedDeskBlotterResults" + scrub + ".json")
+    }
+    else {
+      fs.readFile("cache/cachedDeskBlotterResults" + scrub + ".json", (err, data) => res.end(data));
+      return;
+    }
+  }
+  catch(e) {
+    //...
+  }
+
   loader('http://iic.tulsacounty.org/srsreports/Desk%20Blotter.pdf')
     .then(parser(blotterPageParser(scrub)))
+    .then(data => {fs.writeFileSync("cache/cachedDeskBlotterResults" + scrub + ".json", JSON.stringify(data)); return data; })
     .then(data => res.end(JSON.stringify(data)))
     .catch(err => res.end(JSON.stringify(err)));
 }
@@ -24,8 +46,27 @@ exports.inmateBookingGET = function(args, req, res, next) {
         headers: []
     }).endsWith(x));
 
+
+  try{
+    var stat = fs.statSync("cache/cacheInmateBookingResults" + scrub + ".json");
+    var ctime = new Date(stat.ctime);
+    var now = new Date();
+
+    if((now - ctime) > 3600000) {
+      fs.unlinkSync("cache/cacheInmateBookingResults" + scrub + ".json")
+    }
+    else {
+      fs.readFile("cache/cacheInmateBookingResults" + scrub + ".json", (err, data) => res.end(data));
+      return;
+    }
+  }
+  catch(e) {
+    //...
+  }
+
   loader('http://iic.tulsacounty.org/expInmateBookings/Export')
     .then(parser(inmateBookingPageParser(scrub)))
+    .then(data => {fs.writeFileSync("cache/cacheInmateBookingResults" + scrub + ".json", JSON.stringify(data)); return data; })
     .then(data => res.end(JSON.stringify(data)))
     .catch(err => res.end(JSON.stringify(err)));
 }
